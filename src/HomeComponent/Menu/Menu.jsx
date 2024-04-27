a3ad11aaa018f1cd36ff8689290673ce573edde1
mahmudulhasanprodan <mahmudulhasan8627@gmail.com>
2024-04-27 07:24:02 +0600

    catagories

--- src/HomeComponent/Menu/Menu.jsx
+++ src/HomeComponent/Menu/Menu.jsx
@@ -16,6 +16,14 @@ const Menu = () => {
             <Flex className={"justify-between items-center pt-16 sm:py-10"}>
                 <Flex className={"items-center gap-x-3 px-3 sm:px-0 md:px-3"}>
                 <HiBars3BottomLeft className="text-2xl cursor-pointer"/>
+                <div className="absolute bg-red-300 left-3 z-10 top-32">
+                  <ul>
+                    <li className="py-2 px-24 hover:bg-black hover:text-white">Home</li>
+                    <li className="py-2 px-24 hover:bg-black hover:text-white">Home</li>
+                    <li className="py-2 px-24 hover:bg-black hover:text-white">Home</li>
+                    <li className="py-2 px-24 hover:bg-black hover:text-white">Home</li>
+                  </ul>
+                </div>
                 <p className="Menu__list text-secondary_font_color cursor-pointer hidden sm:block">Shop by Category</p>
                 </Flex>
                 <div>
@@ -25,8 +33,12 @@ const Menu = () => {
                     />
                 </div>
                 <Flex className={"items-center sm:gap-x-1 cursor-pointer px-3 sm:px-0 md:pr-3"}>
-                    <div>
+                    <div className="relative">
                         <FaUser />
+                        <div className="absolute z-10 bg-red-400 right-6">
+                          <h2 className="py-2 hover:bg-black hover:text-white text-center">My Account</h2>
+                          <p className="py-2 hover:bg-black hover:text-white text-center">Logout</p>
+                        </div>
                     </div>
                     <div>
                         <FaCaretDown />
